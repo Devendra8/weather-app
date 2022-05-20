d71158f609c2c5803bd80a71fc4e5ea6b07854d5
Devendra Patil <devendrapatil152@gmail.com>
2022-05-20 12:23:19 +0530

    added react project

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import MainDoc from './components/MainDoc';
+
+function App() {
+  return (
+    <div className="App">
+      <MainDoc />
+    </div>
+  );
+}
+
+export default App;
